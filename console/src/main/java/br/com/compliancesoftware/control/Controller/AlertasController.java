e938b8c7137a08987712d4743b8138636db4f0c8
Douglas Fernandes <douglasf.filho@gmail.com>
2017-02-06 07:49:58 -0300

    SNAPSHOT VERSÃO 1.0.0
    Atualização do backend

--- console/src/main/java/br/com/compliancesoftware/control/Controller/AlertasController.java
+++ console/src/main/java/br/com/compliancesoftware/control/Controller/AlertasController.java
@@ -3,6 +3,7 @@ package br.com.compliancesoftware.control.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.compliancesoftware.Model.HtmlTableBuilder;
+import br.com.compliancesoftware.Model.Params;
+import br.com.compliancesoftware.Model.Params.DateFormat;
 import br.com.compliancesoftware.control.dao.AlertasDao;
 import br.com.compliancesoftware.control.dao.LogsDao;
 import br.com.compliancesoftware.model.Alerta;
@@ -37,12 +40,20 @@ public class AlertasController
 	
 	private static String mensagem = null;
 	
+	private List<Alerta> listaAlertas;
+	
+	/**
+	 * Lista todos os alertas do sistema.
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping("listaAlertas")
 	public String listaAlertas(Model model, HttpSession session) {
 		Perfil usuario = (Perfil)session.getAttribute("usuario");
 		
 		int qtdAlertas = alertasDao.conta();
-		List<Alerta> listaAlertas = alertasDao.lista();
+		listaAlertas = alertasDao.lista();
 		ArrayList<Alerta> beanList = new ArrayList<Alerta>();
 		beanList.addAll(listaAlertas);
 		
@@ -51,6 +62,9 @@ public class AlertasController
 		
 		HtmlTableBuilder builder = new HtmlTableBuilder("center-align", null, "Tabela de alertas do sistema.", "Alertas do Sistema", beanList);
 		builder.setUpdateAction(true);
+		Params params = new Params();
+		params.setFormatoDeData(DateFormat.DMYHM);
+		builder.setParams(params);
 		if(beanList != null && beanList.size() > 0)
 			model.addAttribute("listaAlertas", builder.getTableOrderBy("1", "0"));
 		
@@ -62,31 +76,49 @@ public class AlertasController
 		return "alertas/listaAlertas";
 	}
 	
+	/**
+	 * Modifica a visibilidade de um determinado alerta.
+	 * @param id = id do alerta a modificar.
+	 * @param response = resposta dada à requisição que deve ser usada via Ajax.
+	 */
 	@RequestMapping("atualizarAlerta")
-	public String atualizarAlerta(Long id, Model model, HttpSession session) {
+	public void atualizarAlerta(Long id, HttpServletResponse response) {
 		mensagem = alertasDao.modificaVisibilidade(id);
+		System.out.println(mensagem);
 		
-		Perfil usuario = (Perfil)session.getAttribute("usuario");
-		
-		int qtdAlertas = alertasDao.conta();
-		List<Alerta> listaAlertas = alertasDao.lista();
+		listaAlertas = alertasDao.lista();
 		ArrayList<Alerta> beanList = new ArrayList<Alerta>();
 		beanList.addAll(listaAlertas);
 		
-		model.addAttribute("qtdAlertas",qtdAlertas);
-		model.addAttribute("usuario",usuario);
+		HtmlTableBuilder builder = new HtmlTableBuilder("center-align", null, "Tabela de alertas do sistema.", "Alertas do Sistema", beanList);
+		builder.setUpdateAction(true);
+		Params params = new Params();
+		params.setFormatoDeData(DateFormat.DMYHM);
+		builder.setParams(params);
+		if(beanList != null && beanList.size() > 0)
+			builder.writeTable(response, builder.getTableOrderBy("1", "0"));
+	}
+	
+	private boolean ordemCrescente = true;
+	@RequestMapping("getAlertaTableOrderBy")
+	public void reordenaTabela(String field, HttpServletResponse response) {
+		ArrayList<Alerta> beanList = new ArrayList<Alerta>();
+		beanList.addAll(listaAlertas);
 		
 		HtmlTableBuilder builder = new HtmlTableBuilder("center-align", null, "Tabela de alertas do sistema.", "Alertas do Sistema", beanList);
 		builder.setUpdateAction(true);
-		if(beanList != null && beanList.size() > 0)
-			model.addAttribute("listaAlertas", builder.getTableOrderBy("1", "0"));
-		
-		if(mensagem != null) {
-			model.addAttribute("mensagem",mensagem);
-			mensagem = null;
+		Params params = new Params();
+		params.setFormatoDeData(DateFormat.DMYHM);
+		builder.setParams(params);
+		if(beanList != null && beanList.size() > 0){
+			ordemCrescente = !ordemCrescente;
+			String ordem = "0";
+			if(ordemCrescente)
+				ordem = "0";
+			else
+				ordem = "1";
+			builder.writeTable(response, builder.getTableOrderBy(field, ordem));
 		}
-		
-		return "alertas/listaAlertas";
 	}
 	
 }
--- console/src/main/java/br/com/compliancesoftware/control/Controller/LogsController.java
+++ console/src/main/java/br/com/compliancesoftware/control/Controller/LogsController.java
@@ -3,6 +3,7 @@ package br.com.compliancesoftware.control.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.compliancesoftware.Model.HtmlTableBuilder;
+import br.com.compliancesoftware.Model.Params;
+import br.com.compliancesoftware.Model.Params.DateFormat;
 import br.com.compliancesoftware.control.dao.AlertasDao;
 import br.com.compliancesoftware.control.dao.LogsDao;
 import br.com.compliancesoftware.control.dao.PerfilDao;
@@ -40,6 +43,7 @@ public class LogsController {
 	private LogsDao logsDao;
 	
 	private static String mensagem = null;
+	private List<Log> listaLogs;
 	
 	@RequestMapping("listaLogs")
 	public String listaLogs(Model model, HttpSession session) {
@@ -54,14 +58,39 @@ public class LogsController {
 		Perfil usuario = (Perfil)session.getAttribute("usuario");
 		model.addAttribute("usuario",usuario);
 		
-		List<Log> listaLogs = logsDao.lista(null, null);
+		listaLogs = logsDao.lista(null, null);
 		ArrayList<Log> beanList = new ArrayList<Log>();
 		beanList.addAll(listaLogs);
 		
 		HtmlTableBuilder builder = new HtmlTableBuilder(null, null, "Tabela de logs do sistema", "Logs do Sistema", beanList);
+		Params params = new Params();
+		params.setFormatoDeData(DateFormat.DMYHM);
+		builder.setParams(params);
 		if(beanList != null && beanList.size() > 0)
 			model.addAttribute("listaLogs",builder.getTable());
 		
 		return "logs/listaLogs";
 	}
+	
+	private boolean ordemCrescente = true;
+	@RequestMapping("getLogTableOrderBy")
+	public void reordenaTabela(String field, HttpServletResponse response) {
+		ArrayList<Log> beanList = new ArrayList<Log>();
+		beanList.addAll(listaLogs);
+		
+		HtmlTableBuilder builder = new HtmlTableBuilder(null, null, "Tabela de logs do sistema", "Logs do Sistema", beanList);
+		Params params = new Params();
+		params.setFormatoDeData(DateFormat.DMYHM);
+		builder.setParams(params);
+		if(beanList != null && beanList.size() > 0){
+			ordemCrescente = !ordemCrescente;
+			String ordem = "0";
+			if(ordemCrescente)
+				ordem = "0";
+			else
+				ordem = "1";
+			builder.writeTable(response, builder.getTableOrderBy(field, ordem));
+		}
+	}
+	
 }
